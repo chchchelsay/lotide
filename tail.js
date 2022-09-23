ec192fd4fc3931f90efec0a696d955c32f26425c
Chelsea Dwarika <chelseakristina@live.com>
2022-09-23 20:25:36 +0000

    Tail exercise

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰🥰🥰 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤪🤪🤪 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//function tail returns everything except for the first item (head) of an array - puts into a new array vs changing original.
+const tail = function(array) {
+  let newArray = array.slice(1);
+  console.log(newArray);
+};
+
+const words = ["Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
