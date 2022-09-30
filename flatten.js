995439e3789b1f9d0a66988c07d24350b841cfbb
Chelsea Dwarika <chelseakristina@live.com>
2022-09-30 07:59:06 +0000

    flatten

---
+++ flatten.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  if  ((array1.length === array2.length) &&
+        array1.every((val, i) => val === array2[i])) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+const assertArraysEqual = function(eqArrays, array1, array2) {
+  if (eqArrays === true) {
+    console.log("These are equal!");
+  } else if (eqArrays === false) {
+    console.log("Not a match!");
+  }
+};
+
+
+const flatten = function(nestedArray) {
+
+  for (let i of nestedArray) {
+    Array.isArray(i);
+    if (true) {
+      let flatArray = (nestedArray.flat());
+      return (flatArray);
+    }
+  }
+};
+
+
+flatten([1, 2, [3, 4], 5, [6]]);
