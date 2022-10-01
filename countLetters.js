a3738699a23417daea60315e4442f6635054bdc2
Chelsea Dwarika <chelseakristina@live.com>
2022-10-01 04:59:27 +0000

    objects are cool

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰🥰🥰 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤪🤪🤪 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//takes in a string of letters (sentence) & returns count of each of the letters
+
+const countLetters = function(string) {
+  const numLetter = {};
+  for (const i of string) {
+    numLetter[i] = numLetter[i] ? numLetter[i] + 1 : 1;
+  }
+  return numLetter;
+};
+
+
+
+
+//TEST CASES
+
+//countLetters("lighthouse in the house");
+//l: 1, i: 2, g: 1, h: 4, t: 2, …}
+
+//console.log(countLetters('LHL'));
+
+/*const slogan = ("lighthouse in the house");
+const numLetter= countLetters(slogan);
+assertEqual(numLetter["h"], 4);
+*/
