ddd277d594ed95c3b320a5c08f71940847f7fbb9
Chelsea Dwarika <chelseakristina@live.com>
2022-10-03 15:45:52 +0000

    keybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰🥰🥰 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤪🤪🤪 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//returns first key which contains the given value otherwise undefined
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+  let result;
+  for (let i of keys) {
+    if (object[i] === value) {
+      result = i;
+    }
+  }
+  return result;
+};
+
+//test case
+const letters = {
+  A: "ONE",
+  B: "TWO",
+  C:  "THREE"
+};
+
+
+console.log(findKeyByValue(letters, "ONE"));
+assertEqual(findKeyByValue(letters, "ONE"), "A");
+assertEqual(findKeyByValue(letters, "ONE"), undefined);
