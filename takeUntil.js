59155763ce774be13d428e87673d51081f8b8d83
Chelsea Dwarika <chelseakristina@live.com>
2022-10-08 12:56:41 +0000

    takeuntil

---
+++ takeUntil.js
@@ -0,0 +1,21 @@
+const takeUntil = function(array, condition) {
+ let results = [];
+
+  for (let item of array) {
+    if (condition(item)) break; 
+    results.push(item);
+  }
+
+  return results;
+};
+
+//test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
