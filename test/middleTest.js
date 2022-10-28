9fc0383aec88efd3a1efff615ca9ad459fd36708
Chelsea Dwarika <chelseakristina@live.com>
2022-10-28 18:35:32 +0000

    final

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,4 @@
-const assert = require('chai').assert;const assert = require('chai').assert;
+const assert = require('chai').assert;
 const middle   = require('../middle');
 
 describe("#middle", () => {
